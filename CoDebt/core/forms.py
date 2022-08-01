ccc0078398edec4dabf8a681582c70c12b263254
GitWithLucky <luckyikenkwocha@gmail.com>
2022-08-01 02:21:20 +0100

    Added form features

---
+++ CoDebt/core/forms.py
@@ -0,0 +1,29 @@
+from dataclasses import field
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from core.models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.name = self.cleaned_data['name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+        return user
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email',
+            'name',
+            'school_name',
+            'display_picture',
+            'password',
+            )
+    
--- CoDebt/core/models.py
+++ CoDebt/core/models.py
@@ -1,59 +1,62 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from core.managers import CustomUserManager
 from django.conf import settings
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    username= None
-    name=models.CharField(max_length=100)
-    email=models.EmailField(unique=True, verbose_name='Email Address', max_length=255)
-    is_student=models.BooleanField(default=False)
-    is_school_admin=models.BooleanField(default=False)
-    is_guardian=models.BooleanField(default=False)
-    school_name=models.CharField()
-    #display_picture=models.URLField(max_length=100, default='')
+    username = None
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, verbose_name='Email Address', max_length=255)
+    is_student = models.BooleanField(default=False)
+    is_school_admin = models.BooleanField(default=False)
+    is_guardian = models.BooleanField(default=False)
+    school_name = models.CharField(max_length=50, blank=True)
+    display_picture = models.ImageField(upload_to='uploads', default='', blank=True)
 
-    USERNAME_FIELD= 'email'
-    REQUIRED_FIELDS= []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
 
     class Meta:  
-        ordering= ['email']
-        verbose_name= 'User'
+        ordering = ["-email"]
+        verbose_name = 'User'
 
     def __str__(self):
-        return self.email
+        return self.name
 
 
 class Debtors(models.Model):
-    posted_by=models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    first_name=models.CharField(max_length=50)
-    last_name=models.CharField(max_length=50)
-    school_name=models.CharField(max_length=100)
-    student_id=models.CharField(max_length=20, unique=True)
-    outstanding_fees=models.PositiveIntegerField()
-    is_contending_debt=models.BooleanField(default=False)
+    posted_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    school_name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=20, unique=True)
+    outstanding_fees = models.PositiveBigIntegerField()
+    is_contending_debt = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.first_name,self.last_name
+        return "%s %s" % (self.first_name,self.last_name)
 
 
 class Comment(models.Model):
-    user_commenting=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    posted_on=models.ForeignKey(Debtors, on_delete=models.CASCADE)
-    comment=models.TextField()
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
+    user_commenting = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    posted_on = models.ForeignKey(Debtors, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.comment[:10]
 
 
 class Contention(models.Model):
-    user_contending=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    contending_on=models.ForeignKey(Debtors, on_delete=models.CASCADE)
-    reason= models.TextField()
-    created=models.DateTimeField(auto_now_add=True)
-    evidence=models.FileField()
+    user_contending = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    contending_on = models.ForeignKey(Debtors, on_delete=models.CASCADE)
+    reason = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    evidence = models.FileField(upload_to='uploads')
 
     def __str__(self):
-        return self.contending_on
+        return self.reason
