d572954baf68796f5391deee9c887b60265f5e3e
Sharon Agwah <47524325+sharonij@users.noreply.github.com>
2022-08-04 16:59:51 +0100

    Update views.py

--- CoDebt/core/views.py
+++ CoDebt/core/views.py
@@ -1,7 +1,29 @@
+from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from .forms import UserCreationForm, RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+     form == RegisterForm()
+     if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            messages.success(request, 'Account was created for ' + username)
+            user = authenticate(username=username, password=password)
+            #login(request,user)
+            return redirect('index')
+     else:
+        print('Form is not valid')
+        messages.error(request, 'Error Processing Your Request')
+        form = RegisterForm()
+        return render (request, './signup.html', {'form': form})  
