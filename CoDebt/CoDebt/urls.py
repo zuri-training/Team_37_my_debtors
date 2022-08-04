a7d3898051628a581654e45a9a7a14338c1f6a4d
Sharon Agwah <47524325+sharonij@users.noreply.github.com>
2022-08-04 17:02:34 +0100

    Update urls.py

--- CoDebt/CoDebt/urls.py
+++ CoDebt/CoDebt/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views as core_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('register/', core_views.register, name='register')
 ]
 
 if settings.DEBUG:
