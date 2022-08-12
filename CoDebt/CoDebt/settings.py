db8267069483c3d16cf02c9d1ffe5cbea3129978
GitWithLucky <luckyikenkwocha@gmail.com>
2022-08-12 14:59:26 +0100

    Done

--- CoDebt/CoDebt/settings.py
+++ CoDebt/CoDebt/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'CoDebt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE':'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER':os.getenv('DB_USERNAME'),
-#         'PASSWORD':os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USERNAME'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
+    }
+}
 
 
 
