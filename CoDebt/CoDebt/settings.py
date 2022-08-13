26111a4e6c3f0c86d9ebf1f19f54bf94891d7ee6
Agbani123 <okaforjulia2016@gmail.com>
2022-08-13 10:58:08 +0100

    done

--- CoDebt/CoDebt/settings.py
+++ CoDebt/CoDebt/settings.py
@@ -167,8 +167,8 @@ cloudinary.config(
 )
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'console.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL')
